a2b1e04783bd96dfef4f85d69412f5b5e0d843a2
John <johnbond@live.nl>
2023-01-06 15:52:55 +0100

    Added seperate sendmessage func

--- src/sender.go
+++ src/sender.go
@@ -16,9 +16,38 @@ var (
 	client     *twilio.RestClient
 )
 
-func SendMessage(msg helpers.GroupMessage) {
+func SendGroupMessage(msg helpers.GroupMessage) {
 	params := openapi.CreateMessageParams{}
-	params.SetMessagingServiceSid("{MessagingServiceID}")
+	params.SetMessagingServiceSid("MGc04be72a20e29d49ecd83f987720b9c4")
+
+	// check if scheduled
+	if msg.ScheduledAt.Unix() > (time.Now().Local().Unix() + 900) {
+		fmt.Println("schedule sms")
+		params.SetSendAt(msg.ScheduledAt)
+		params.SetScheduleType("fixed")
+	}
+
+	// sms params
+	params.SetTo(msg.ToPhoneNumber)
+	params.SetFrom(msg.FromPhoneNumber)
+	params.SetBody(msg.Message)
+
+	// send sms
+	resp, err := client.Api.CreateMessage(&params)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		if resp.Sid != nil {
+			fmt.Println(*resp.Sid)
+		} else {
+			fmt.Println(resp.Sid)
+		}
+	}
+}
+
+func SendLocationMessage(msg helpers.LocationMessage) {
+	params := openapi.CreateMessageParams{}
+	params.SetMessagingServiceSid("MGc04be72a20e29d49ecd83f987720b9c4")
 
 	// check if scheduled
 	if msg.ScheduledAt.Unix() > (time.Now().Local().Unix() + 900) {
@@ -46,8 +75,8 @@ func SendMessage(msg helpers.GroupMessage) {
 }
 
 func init() {
-	accountSid = "{AccountSid}"
-	authToken = "{authToken}"
+	accountSid = "AC7c1bac13c8d94ff8b152a06f74e47d47"
+	authToken = "e3c1d0d2aa98708bf66689a17251c88c"
 
 	client = twilio.NewRestClientWithParams(twilio.ClientParams{
 		Username: accountSid,
@@ -63,18 +92,18 @@ func sender() {
 	//GroupMessage.ScheduledAt = time.Now()
 	GroupMessage.ScheduledAt = time.Date(2023, 1, 6, 15, 18, 0, 0, time.Local)
 	GroupMessage.Message = "test"
-	GroupMessage.FromPhoneNumber = "{FromPhoneNumber}"
-	GroupMessage.ToPhoneNumber = "{ToPhoneNumber}"
-	SendMessage(GroupMessage)
+	GroupMessage.FromPhoneNumber = "+12183199239"
+	GroupMessage.ToPhoneNumber = "+31611523882"
+	SendGroupMessage(GroupMessage)
 
-	// var LocationMessage helpers.LocationMessage
-	// LocationMessage.MessageId = uuid.UUID{}
-	// LocationMessage.LocationId = uuid.UUID{}
-	// LocationMessage.ScheduledAt = time.Now()
-	// LocationMessage.ScheduledAt = time.Date(2023, 1, 6, 15, 18, 0, 0, time.Local)
-	// LocationMessage.Message = "test"
-	// LocationMessage.FromPhoneNumber = "{FromPhoneNumber}"
-	// LocationMessage.ToPhoneNumber = "{ToPhoneNumber}"
-	//SendMessage(LocationMessage)
+	var LocationMessage helpers.LocationMessage
+	LocationMessage.MessageId = uuid.UUID{}
+	LocationMessage.LocationId = uuid.UUID{}
+	LocationMessage.ScheduledAt = time.Now()
+	LocationMessage.ScheduledAt = time.Date(2023, 1, 6, 15, 18, 0, 0, time.Local)
+	LocationMessage.Message = "test"
+	LocationMessage.FromPhoneNumber = "+12183199239"
+	LocationMessage.ToPhoneNumber = "+31611523882"
+	SendLocationMessage(LocationMessage)
 
 }
