e4df6e7f736349882a21ab00969565529f26933a
John <johnbond@live.nl>
2023-01-06 15:36:48 +0100

    Twillio sender changes

--- src/helpers/GroupMessage.go
+++ src/helpers/GroupMessage.go
@@ -7,9 +7,10 @@ import (
 )
 
 type GroupMessage struct {
-	MessageId   uuid.UUID
-	ClassId     uuid.UUID
-	ScheduledAt time.Time
-	Message     string
-	PhoneNumber string
+	MessageId       uuid.UUID
+	ClassId         uuid.UUID
+	ScheduledAt     time.Time
+	Message         string
+	FromPhoneNumber string
+	ToPhoneNumber   string
 }
--- src/sender.go
+++ src/sender.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"sms-consumer/src/helpers"
+	"time"
 
-	"github.com/joho/godotenv"
+	"github.com/google/uuid"
 	"github.com/twilio/twilio-go"
 	openapi "github.com/twilio/twilio-go/rest/api/v2010"
 )
@@ -12,37 +13,41 @@ import (
 var (
 	accountSid string
 	authToken  string
-	fromPhone  string
-	toPhone    string
 	client     *twilio.RestClient
 )
 
-func SendMessage(msg string) {
-
+func SendMessage(msg helpers.GroupMessage) {
 	params := openapi.CreateMessageParams{}
-	params.SetTo(toPhone)
-	params.SetFrom(fromPhone)
-	params.SetBody(msg)
+	params.SetMessagingServiceSid("{MessagingServiceID}")
 
-	response, err := client.Api.CreateMessage(&params)
-	if err != nil {
-		fmt.Printf("error creating and sending message: %s\n", err.Error())
-		return
+	// check if scheduled
+	if msg.ScheduledAt.Unix() > (time.Now().Local().Unix() + 900) {
+		fmt.Println("schedule sms")
+		params.SetSendAt(msg.ScheduledAt)
+		params.SetScheduleType("fixed")
 	}
-	fmt.Printf("Message SID: %s\n", *response.Sid)
+
+	// sms params
+	params.SetTo(msg.ToPhoneNumber)
+	params.SetFrom(msg.FromPhoneNumber)
+	params.SetBody(msg.Message)
+
+	// send sms
+	resp, err := client.Api.CreateMessage(&params)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		if resp.Sid != nil {
+			fmt.Println(*resp.Sid)
+		} else {
+			fmt.Println(resp.Sid)
+		}
+	}
 }
 
 func init() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		fmt.Printf("error loading .env: %s\n", err.Error())
-		os.Exit(1)
-	}
-
-	accountSid = "AC7c1bac13c8d94ff8b152a06f74e47d47"
-	authToken = "e3c1d0d2aa98708bf66689a17251c88c"
-	fromPhone = "+12183199239"
-	toPhone = "+31611523882"
+	accountSid = "{AccountSid}"
+	authToken = "{authToken}"
 
 	client = twilio.NewRestClientWithParams(twilio.ClientParams{
 		Username: accountSid,
@@ -52,7 +57,24 @@ func init() {
 
 func sender() {
 
-	msg := fmt.Sprintf(os.Getenv("MSG"), "James")
-	SendMessage(msg)
+	var GroupMessage helpers.GroupMessage
+	GroupMessage.MessageId = uuid.UUID{}
+	GroupMessage.ClassId = uuid.UUID{}
+	//GroupMessage.ScheduledAt = time.Now()
+	GroupMessage.ScheduledAt = time.Date(2023, 1, 6, 15, 18, 0, 0, time.Local)
+	GroupMessage.Message = "test"
+	GroupMessage.FromPhoneNumber = "{FromPhoneNumber}"
+	GroupMessage.ToPhoneNumber = "{ToPhoneNumber}"
+	SendMessage(GroupMessage)
+
+	// var LocationMessage helpers.LocationMessage
+	// LocationMessage.MessageId = uuid.UUID{}
+	// LocationMessage.LocationId = uuid.UUID{}
+	// LocationMessage.ScheduledAt = time.Now()
+	// LocationMessage.ScheduledAt = time.Date(2023, 1, 6, 15, 18, 0, 0, time.Local)
+	// LocationMessage.Message = "test"
+	// LocationMessage.FromPhoneNumber = "{FromPhoneNumber}"
+	// LocationMessage.ToPhoneNumber = "{ToPhoneNumber}"
+	//SendMessage(LocationMessage)
 
 }
